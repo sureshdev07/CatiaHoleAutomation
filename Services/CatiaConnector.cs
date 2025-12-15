6e6cc4870c45ded838385ce208726273d8862828
sureshdev07 <sureshvijayan29592@gmail.com>
2025-12-15 00:21:01 +0000

    Add project files.

---
+++ Services/CatiaConnector.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using INFITF;
+using MECMOD;
+
+namespace CatiaHoleAutomation.Services
+{
+    public class CatiaConnector
+    {
+        public INFITF.Application CatiaApp { get; set; }
+
+        public PartDocument PartDoc { get;  set; }
+
+        public Part Part { get; set; }
+
+        public void Connect()
+        {
+            try
+            {
+                CatiaApp = (Application)Marshal.GetActiveObject("CATIA.Application");
+
+            }
+            catch (Exception ex)
+            {
+                throw new Exception("CATIA connection failed: " + ex.Message);
+            }
+        }
+    }
+}
---
+++ UI/MainForm.cs
@@ -0,0 +1,19 @@
+﻿using CatiaHoleAutomation.Services;
+using System;
+using System.Windows.Forms;
+
+namespace CatiaHoleAutomation
+{
+    public partial class MainForm : Form
+    {
+        private readonly CatiaConnector _catiaConnector;
+
+        public MainForm()
+        {
+            InitializeComponent();
+            _catiaConnector = new CatiaConnector();
+            _catiaConnector.Connect();
+        }
+
+    }
+}
